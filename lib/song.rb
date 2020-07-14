fa5d3af462539d8dfc477d0a15c78172439b308d
Juan Rivera <juanriv45@gmail.com>
2020-07-14 00:30:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
@@ -52,3 +53,57 @@ class Song
 
 
 end
+=======
+class Song
+attr_accessor :name, :artist, :genre
+@@count = 0
+@@genres = []
+@@artists = []
+  def initialize(name,artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@genres << genre
+    @@artists << artist
+    @@count+=1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genre_hash = {}
+    @@genres.each  do |genre|
+      if genre_hash[genre] then
+        genre_hash[genre] +=1
+      else
+        genre_hash[genre] = 1
+      end
+    end
+    genre_hash
+  end
+
+  def self.artist_count
+    artists_hash = {}
+    @@artists.each  do |artist|
+      if artists_hash[artist] then
+        artists_hash[artist] +=1
+      else
+        artists_hash[artist] = 1
+      end
+    end
+    artists_hash
+  end
+
+end
+>>>>>>> d448da9281eac662cc534eb611e3f83e869a69d1
